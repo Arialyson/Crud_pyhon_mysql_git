42b43672bb544ade663d112add7297122fcd08aa
Arialyson Lima <arialyson@gmail.com>
2025-10-26 01:28:39 -0300

    funções
    as 4 principais funções de interação com banco de dados: create, read, update e delete.

--- crud.py
+++ crud.py
@@ -10,9 +10,37 @@ conexao = mysql.connector.connect(
 cursor = conexao.cursor()
 
 #CREATE
+
+#nome_var = "Kratos"
+#nasc_var = "1988-12-03"
+#na_var = "Grécia"
+#comando = f'INSERT INTO personas (id, nome, nascimento, nacionalidade) VALUES ("","{nome_var}","{nasc_var}","");'
+#cursor.execute(comando)
+#conexao.commit() # edita o banco de dados
+
+# resultado = cursor.fetchall() # ler o banco de dados
+
 #READ
+
+#comando = 'SELECT * FROM  personas'
+#cursor.execute(comando)
+#resultado = cursor.fetchall()
+#print(resultado)
+
 #UPDATE
+
+#nome_vari = "Kratos"
+#na_vari = "Suiça"
+#comando = f'UPDATE personas SET nacionalidade = "{na_vari}" WHERE nome = "{nome_vari}"'
+#cursor.execute(comando)
+#conexao.commit()
+
 #DELETE
+
+nome_vari = "Maria"
+comando = f'DELETE FROM personas WHERE nome = "{nome_vari}"'
+cursor.execute(comando)
+conexao.commit()
 
 cursor.close()
 conexao.close()
